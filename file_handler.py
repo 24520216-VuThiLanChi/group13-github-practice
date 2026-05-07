603a3cec0414a3a4ec99c83a97de78da7a1531de
williambale7 <nqc1842005@gmail.com>
2026-05-08 02:03:20 +0700

    done let's check

--- file_handler.py
+++ file_handler.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import json
+
+FILE_NAME = "tasks.json"
+
+
+def save_tasks(tasks):
+    with open(FILE_NAME, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def load_tasks():
+    try:
+        with open(FILE_NAME, "r") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        return []
